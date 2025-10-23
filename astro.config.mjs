0e41b3d3bbc0f1b1c1ef8fcab8e3b8e23659f90d
jeremy <jeremy.boubee@epitech.com>
2025-10-23 22:33:26 +0200

    🔨 chore(config): add preact svg component support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import preact from '@astrojs/preact';
-import tailwindcss from '@tailwindcss/vite';
+import preact from "@astrojs/preact";
+import svgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss(),
+      svgr({
+        include: "**/*.svg?preact",
+        svgrOptions: {
+          plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+          svgoConfig: {
+            plugins: [
+              "preset-default",
+              "removeTitle",
+              "removeDesc",
+              "removeDoctype",
+              "cleanupIds",
+            ],
+          },
+        },
+      }),
+    ],
   },
 
-  integrations: [preact({ compat: true })]
+  integrations: [preact({ compat: true })],
 });
