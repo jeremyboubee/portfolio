a04a5a389b060880e61ad3f81e974674ba24e1b8
jeremy <jeremy.boubee@epitech.com>
2025-10-26 21:04:07 +0100

    🔨 chore: config astro site & base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jeremyboubee.github.io",
+  base: "/portfolio",
   vite: {
     plugins: [
       tailwindcss(),
